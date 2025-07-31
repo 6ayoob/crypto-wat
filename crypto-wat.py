13118fa47ae819ab6af420e397fbcff9371eb643
6ayoob <tayoob632@gmail.com>
2025-07-31 19:25:09 +0300

    Update crypto-wat.py

--- crypto-wat.py
+++ crypto-wat.py
@@ -110,7 +110,7 @@ async def main_loop():
         except Exception as e:
             logging.error(f"❌ خطأ في الحلقة الرئيسية: {e}")
 
-        await asyncio.sleep(1800)  # كل 30 دقيقة
+        await asyncio.sleep(900)  # كل 30 دقيقة
 
 
 async def main():
