3e3a89460e864c73ce5d67f01fd6618324e02260
6ayoob <tayoob632@gmail.com>
2025-08-02 19:21:01 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,30 +1,9 @@
 # config.py
 
-# توكن بوت التليجرام
-TELEGRAM_TOKEN = "8300868885:AAEx8Zxdkz9CRUHmjJ0vvn6L3kC2kOPCHuk"
-
-# معرف المحادثة (chat_id) الذي سيرسل إليه البوت الرسائل
-TELEGRAM_CHAT_ID = "658712542"
-
-# مفاتيح API الخاصة بمنصة okx (مفتاح API والسر السري)
 OKX_API_KEY = "5df74c21-ab87-4bef-8125-80e07eba1f5b"
 OKX_SECRET_KEY = "AB05EDC855495411BF66A5597FFB23B7"
 OKX_PASSPHRASE = "Ta123456&"
-
-# عدد العملات التي سيتم فحصها من الأعلى حجم تداول (مثلاً 50)
-TOP_COINS = 50
-
-# نسبة المال الذي تريد المخاطرة به في كل صفقة (مثلاً 10% = 0.10)
-TRADE_AMOUNT_PERCENT = 0.10
-
-# نسبة وقف الخسارة (مثلاً 3%)
-STOP_LOSS_PERCENT = 3
-
-# حد أدنى وأعلى لجني الأرباح (مثلاً من 3% إلى 8%)
-TAKE_PROFIT_MIN = 3
-TAKE_PROFIT_MAX = 8
-
-# فترة التحقق بين كل فحص بالساعات (مثلاً 25 دقيقة = 25)
-CHECK_INTERVAL_MINUTES = 25
+TELEGRAM_TOKEN = "8300868885:AAEx8Zxdkz9CRUHmjJ0vvn6L3kC2kOPCHuk"
+TELEGRAM_CHAT_ID = "658712542"
 
 ALLOWED_USER_IDS = [658712542]
