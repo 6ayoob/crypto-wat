b24f3cda205fb64f0774c590a97625bf11cb4a42
6ayoob <tayoob632@gmail.com>
2025-08-05 15:24:20 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,9 +1,7 @@
 # telegram_bot.py
 import requests
-
-TELEGRAM_TOKEN = "7863509137:AAHBuRbtzMAOM_yBbVZASfx-oORubvQYxY8"
-TELEGRAM_CHAT_ID = 6587216  # يمكنك تغيير المعرف إن كان مختلفًا
-
+TELEGRAM_TOKEN = "8300868885:AAEx8Zxdkz9CRUHmjJ0vvn6L3kC2kOPCHuk"
+TELEGRAM_CHAT_ID = "658712542"
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
     payload = {
