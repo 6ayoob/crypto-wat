b354c1854fb53b3cd8da08bb4225ead7a0e18878
6ayoob <tayoob632@gmail.com>
2025-10-27 09:44:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -30,8 +30,7 @@ from config import (
 )
 
 # الاستراتيجية
-from strategy import _build_entry_plan as build_entry_plan
- (
+from strategy import _build_entry_plan as build_entry_plan (
     check_signal, manage_position, load_position, save_position,
     count_open_positions, build_daily_report_text,
     reset_cycle_cache, metrics_format,
